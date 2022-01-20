6e06c097b0601d51ba88686e771601526598def6
TinyLittleWolf13 <j.c.degroot-3@student.utwente.nl>
2022-01-20 15:37:23 +0100

    Added smooth angle function for single joint, commented out 1 function for the robot arm class since it is very easy to misuse.

--- source/robotarm.py
+++ source/robotarm.py
@@ -1,4 +1,5 @@
 from adafruit_servokit import ServoKit
+import time
 
 
 class SingleServo:
@@ -6,6 +7,7 @@ class SingleServo:
         self.debug_mode = debug_mode
         self.port = port
         self.angle = angle
+        # todo: actually move the servo to the angle?
 
         self.minangle = parameters["minangle"]
         self.maxangle = parameters["maxangle"]
@@ -24,6 +26,39 @@ class SingleServo:
             self.angle = self.maxangle
             self.joint.servo[self.port].angle = self.maxangle
 
+    def set_angle_smooth(self, angle, seconds):
+        elapsed = 0
+        start_angle = self.angle()
+        end_angle = self.bound_angle(angle)
+        moving_angle = abs(start_angle - end_angle)
+        current_angle = start_angle
+        dt = .01
+        correction = 1 / (1 - (1 - sigmoid(6)) * 2)
+        while elapsed < seconds:
+            t = (elapsed / seconds)*12 - 6
+            step_size = moving_angle * derivative_sigmoid(t) * ((dt/seconds)*12) * correction
+            if end_angle > start_angle:
+                current_angle += step_size
+                current_angle = min(current_angle, end_angle)
+            if end_angle < start_angle:
+                current_angle -= step_size
+                current_angle = max(current_angle, end_angle)
+            self.angle = self.bound_angle(current_angle)
+            self.joint.servo[self.port].angle = self.bound_angle(current_angle)
+            elapsed += dt
+            time.sleep(dt)
+        self.angle = self.bound_angle(angle)
+        self.joint.servo[self.port].angle = self.bound_angle(angle)
+
+    def bound_angle(self, angle):
+        if self.minangle <= angle <= self.maxangle:
+            return angle
+        elif angle < self.minangle:
+            return self.minangle
+        elif angle > self.maxangle:
+            return self.maxangle
+
+
     def get_angle(self):
         return self.angle
 
@@ -34,6 +69,7 @@ class DualServo:
         self.port1 = ports[0]
         self.port2 = ports[1]
         self.angle = angle
+        # todo: move the servo to the angle?
 
         self.minangle = parameters["minangle"]
         self.maxangle = parameters["maxangle"]
@@ -48,13 +84,13 @@ class DualServo:
             self.joint.servo[self.port1].angle = angle
             self.joint.servo[self.port2].angle = 180 - angle
         elif angle < self.minangle:
-            self.angle = 0
-            self.joint.servo[self.port1].angle = 0
-            self.joint.servo[self.port2].angle = 180
+            self.angle = self.minangle
+            self.joint.servo[self.port1].angle = self.minangle
+            self.joint.servo[self.port2].angle = 180 - self.minangle
         elif angle > self.maxangle:
-            self.angle = 180
-            self.joint.servo[self.port1].angle = 180
-            self.joint.servo[self.port2].angle = 0
+            self.angle = self.maxangle
+            self.joint.servo[self.port1].angle = self.maxangle
+            self.joint.servo[self.port2].angle = 180 - self.maxangle
 
     def get_angle(self):
         return self.angle
@@ -69,13 +105,30 @@ class RobotArm:
         self.grabber = SingleServo(parameters[4], ports[4], position[4])
 
     def set_angle(self, angle):
-        self.base.set_angle(angle)
-        self.shoulder.set_angle(angle)
-        self.elbow.set_angle(angle)
-        self.wrist.set_angle(angle)
-        self.grabber.set_angle(angle)
+        """
+            Disabled the function since it could very likely damage the arm if the minimum angle and maximum angle of the joints are not properly set.
+        """
+        pass
+        # self.base.set_angle(angle)
+        # self.shoulder.set_angle(angle)
+        # self.elbow.set_angle(angle)
+        # self.wrist.set_angle(angle)
+        # self.grabber.set_angle(angle)
 
     def get_angle(self):
         angels = [self.base.get_angle(), self.shoulder.get_angle(), self.elbow.get_angle(), self.wrist.get_angle(),
                   self.grabber.get_angle()]
         return angels
+
+import math
+def sigmoid(x):
+    """
+        Basic implementation of the sigmoid function, may not work well with large negative numbers.
+    """
+    sigmoid = 1 / (1 + math.exp(-x))
+    return sigmoid
+
+def derivative_sigmoid(x):
+    sigmoid = sigmoid(x)
+    derivative = sigmoid * (1-sigmoid)
+    return derivative
