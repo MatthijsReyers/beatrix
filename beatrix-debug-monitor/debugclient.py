b282a918de085537fbac95f35da256c4237b76f4
MatthijsReyers <matthijscrafter@gmail.com>
2022-01-24 09:53:29 +0100

    Refactored DebugClient to use new socket classes

--- beatrix-debug-monitor/debugclient.py
+++ beatrix-debug-monitor/debugclient.py
@@ -1,4 +1,4 @@
-from socket import socket, AF_INET, SOCK_STREAM, SOCK_DGRAM, SO_SNDBUF, SOL_SOCKET
+from lib.clientsock import ClientSocket
 import pickle, cv2, json
 
 VIDEO_BUFFER_SIZE = 1000000
@@ -10,31 +10,29 @@ RASP_IP = '127.0.0.1'
 
 class DebugClient():
     def __init__(self):
-        self.control_socket = socket(AF_INET, SOCK_STREAM)
+        self.control_socket = ClientSocket(RASP_IP, CONTROL_PORT)
+        self.video_socket = ClientSocket(RASP_IP, VIDEO_PORT)
 
-        self.video_socket = socket(AF_INET, SOCK_DGRAM)
-        self.video_socket.bind(('', VIDEO_PORT))
-        self.video_socket.settimeout(0.4)
-
-        self.reconnecting = False
-
-    def connect(self):
-        print('[*] Connecting to control server')
-        self.running = True
-        self.__reconnect()
+    def start(self):
+        print('[*] Connecting to control server.')
+        self.control_socket.start()
+        self.video_socket.start()
 
     def stop(self, event):
         print('[*] Exiting, closing sockets.')
-        self.running = False
         self.control_socket.close()
+        self.video_socket.close()
 
     def is_connected(self):
-        return self.control_socket and (self.control_socket.fileno() != -1)
+        return self.control_socket.is_connected() and self.video_socket.is_connected()
 
-    def recieve_video(self):
-        data, addr = self.video_socket.recvfrom(VIDEO_BUFFER_SIZE)
-        data = pickle.loads(data)
-        return cv2.imdecode(data, cv2.IMREAD_COLOR)
+    def recieve_video(self) -> (bool, any):
+        okay, data = self.video_socket.receive(buffer_size=1000000)
+        if okay:
+            data = pickle.loads(data)
+            print('got video...')
+            return (True, cv2.imdecode(data, cv2.IMREAD_COLOR))
+        return (False, None)
         
     def recieve_command(self):
         try:
@@ -51,7 +49,7 @@ class DebugClient():
 
     def send_set_position_cmd(self, position: (float, float, float)):
         """ Sends a command to set the position of the robot arm. """
-        self.__send_cmd({
+        self._send_cmd({
             'type': 'SET_POSITION',
             'data': {
                 'x': position[0],
@@ -62,50 +60,36 @@ class DebugClient():
 
     def send_get_position_cmd(self):
         """ Sends a command to get the current position of the robot arm. """
-        self.__send_cmd({
+        self._send_cmd({
             'type': 'GET_POSITION',
             'data': {}
         })
 
     def send_go_home_cmd(self):
         """ Sends a command to move the robot arm to its home position. """
-        self.__send_cmd({
+        self._send_cmd({
             'type': 'GO_HOME',
             'data': {}
         })
 
-    def __send_cmd(self, cmd):
+    def _send_cmd(self, cmd):
         try:
             packet = json.dumps(cmd)
             self.control_socket.send(packet)
         except Exception as e:
             print(e)
 
-    def __reconnect(self):
-        if not self.reconnecting:
-            while self.running and not self.is_connected():
-                self.reconnecting = True
-                try:
-                    self.control_socket = socket(AF_INET, SOCK_STREAM)
-                    self.control_socket.connect((RASP_IP, CONTROL_PORT))
-                    print('fileno:', self.control_socket.fileno())
-                    self.reconnecting = False
 
-                except Exception as e:
-                    print('Caught exception:')
-                    print(self.control_socket.fileno())
-                    print(e)
-
-            self.reconnecting = False
-
-
-# if __name__ == '__main__':
+if __name__ == '__main__':
     
-#     client = SocketClient()
-#     client.start()
+    client = DebugClient()
+    client.start()
 
-#     while True:
-#         frame = client.recieve_video()
-#         cv2.imshow('server', frame)
-#         if cv2.waitKey(10) == 13:
-#             exit(0)
+    while True:
+        okay, frame = client.recieve_video()
+        if okay:
+            cv2.imshow('server', frame)
+            if cv2.waitKey(10) == 13:
+                exit(0)
+        else:
+            print('not okay!')
