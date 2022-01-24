7aa9c4c9b07ffa05c00ac90077f6f3409c735fc0
MatthijsReyers <matthijscrafter@gmail.com>
2022-01-24 10:36:02 +0100

    Added proper threads shutdown cleanup stuff

--- beatrix-debug-monitor/__main__.py
+++ beatrix-debug-monitor/__main__.py
@@ -1,4 +1,4 @@
-from mainwindow import MainWindow
+from gui.mainwindow import MainWindow
 from debugclient import DebugClient
 import ikpy, gi
 
@@ -8,7 +8,14 @@ from gi.repository import Gtk
 client = DebugClient()
 client.connect()
 
+def stopapp():
+    """ This function is responsible for propagating the stop signal to the various modules and threads 
+    of the application. """
+    logger.log('Exiting application.')
+    client.stop()
+
 window = MainWindow(client)
+window.connect("destroy", stopapp)
 window.connect("destroy", Gtk.main_quit)
 window.show_all()
 window.start()
