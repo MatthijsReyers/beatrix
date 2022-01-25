94496b31fa4abfe86a8da96f8429e0cc29c54858
MatthijsReyers <matthijscrafter@gmail.com>
2022-01-25 09:13:37 +0100

    Added --no-pi default to True for windows/manjaro systems

--- beatrix-controller/__main__.py
+++ beatrix-controller/__main__.py
@@ -1,8 +1,11 @@
-import argparse, sys
+import sys, platform, argparse
+
+# Are we running on a Raspberry Pi?
+is_pi = platform.system() != 'Windows' and 'MANJARO' not in platform.release()
 
 # Setup commandline arguments parser.
 parser = argparse.ArgumentParser(description='Controller software for the Beatrix robot arm.')
-parser.add_argument('--no-pi', default=False, action='store_true',
+parser.add_argument('--no-pi', default=is_pi, action='store_true',
                     help='Don\'t connect to GPIO pins, useful for when you don\'t want to run this\
                         software on a Raspberry Pi.')
 
@@ -13,8 +16,10 @@ args = parser.parse_args(sys.argv[1:])
 from robotarm import RobotArm
 from camera import Camera
 from controller import Controller
+from debugserver import DebugServer
 
 # Initialize system components.
-camera = Camera(debug_mode=args.no_debug)
-robotarm = RobotArm(debug_mode=args.no_debug)
-controller = Controller(camera, robotarm)
+camera = Camera(debug_mode=args.no_pi)
+server = DebugServer()
+# robotarm   = RobotArm(debug_mode=args.no_pi)
+# controller = Controller(camera, robotarm)
