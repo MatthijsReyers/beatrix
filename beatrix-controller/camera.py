000cccb0d6bc59bde0fdb3032bb8047ee866b472
MatthijsReyers <matthijscrafter@gmail.com>
2022-01-27 12:20:59 +0100

    Added thread to camera

--- beatrix-controller/camera.py
+++ beatrix-controller/camera.py
@@ -1,5 +1,34 @@
+from threading import Thread
 
 class Camera():
-    def __init__(self, debug_mode:bool=False):
-        self.debug_mode = debug_mode
+    def __init__(self, debug_server):
+        self.debug_server = debug_server
 
+        self.cap = cv2.VideoCapture(0)
+        self.cap.set(cv2.CAP_PROP_FPS, 2)
+
+        self.running = False
+
+    def start(self):
+        self.running = False
+        self.camera_thread = Thread(
+            target=self.__camera_thread, 
+            daemon=True,
+            args=())
+        self.camera_thread.start()
+
+    def stop(self):
+        self.running = False
+        if self.camera_thread != None:
+            self.camera_thread.join()
+
+    def __camera_thread(self):
+        try:
+            while self.running:
+                okay, frame = self.cap.read()
+                if okay:
+                    self.debug_server.send_video_frame(frame)
+                self.running = False
+        finally:
+            self.cap.release()
+    
