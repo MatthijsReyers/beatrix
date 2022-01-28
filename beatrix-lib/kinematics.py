55cb7af3f6934334cd0888399d16c4abf225e31a
Guus Grievink <64guusgeluk@gmail.com>
2022-01-28 13:23:07 +0100

    Degrees fix

--- beatrix-lib/kinematics.py
+++ beatrix-lib/kinematics.py
@@ -3,6 +3,7 @@ from abc import abstractmethod
 from ikpy.chain import Chain
 from lib.constants import *
 from enum import Enum
+from math import degrees
 
 class WristOrientation(Enum):
     UNSET = 0
@@ -37,16 +38,19 @@ class IkPyKinematics(Kinematics):
             Angles dictionary:  {JOINT_ID: anglex, JOINT_ID2: anglexx, etc...} with angles in degrees
 
         """
+
+
         solution_angles = self.chain.inverse_kinematics(position)
         new_angles = {
-            BASE_JOINT_ID: (solution_angles[1] + 90),  # +90 for compensation of chain bounds (-90, 180)
+            BASE_JOINT_ID: degrees(solution_angles[1] + 90),  # +90 for compensation of chain bounds (-90, 180)
             # instead of (0, 270)
-            SHOULDER_JOINT_ID: solution_angles[2],
-            ELBOW_JOINT_ID: solution_angles[3],
-            WRIST_JOINT_ID: solution_angles[4],
-            WRIST_TURN_JOINT_ID: solution_angles[5]
+            SHOULDER_JOINT_ID: degrees(solution_angles[2]),
+            ELBOW_JOINT_ID: degrees(solution_angles[3]),
+            WRIST_JOINT_ID: degrees(solution_angles[4]),
+            WRIST_TURN_JOINT_ID: degrees(solution_angles[5])
         }
         return new_angles
 
     def forward(self, angles: list) -> Tuple[float, float, float]:
+        self.chain.forward_kinematics()
         return [0, 0, 0]
