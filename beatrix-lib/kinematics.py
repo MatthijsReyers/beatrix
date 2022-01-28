d28412a84089fdf37f4a0cc3a41416c0f4d1e03f
matthijsreyers <matthijscrafter@gmail.com>
2022-01-28 12:48:59 +0100

    Rewrote tuple type hints to official style

--- beatrix-lib/kinematics.py
+++ beatrix-lib/kinematics.py
@@ -1,3 +1,4 @@
+from typing import Tuple
 from lib.chain import ik_chain
 from abc import abstractmethod
 from ikpy.chain import Chain
@@ -18,8 +19,8 @@ class IkPyKinematics(Kinematics):
     def __init__(self, chain: Chain):
         self.chain = chain
 
-    def inverse(self, position:(float,float,float)) -> list: # TODO omzetten naar dict
+    def inverse(self, position:Tuple[float, float, float]) -> list: # TODO omzetten naar dict
         return self.chain.inverse_kinematics(position)
 
-    def forward(self, angles:list) -> (float,float,float):
+    def forward(self, angles:list) -> Tuple[float, float, float]:
         return [0,0,0]
