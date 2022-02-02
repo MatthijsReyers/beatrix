8b8223ba5e0907e4667a104f779cf27dc5b7e1e2
Guus Grievink <64guusgeluk@gmail.com>
2022-02-02 14:17:49 +0100

    orig exposure

--- beatrix-controller/camera.py
+++ beatrix-controller/camera.py
@@ -14,7 +14,7 @@ class Camera():
         self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1088)
         self.cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)
         self.cap.set(cv2.CAP_PROP_AUTO_EXPOSURE, 1)  # 1 is manual, 3 is auto
-        self.cap.set(cv2.CAP_PROP_EXPOSURE, 250)
+        self.cap.set(cv2.CAP_PROP_EXPOSURE, 240)
 
         self.running = False
 
