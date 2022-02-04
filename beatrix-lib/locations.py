16573c2f2285357bcb9c3a3536aa2740f96cc168
matthijsreyers <matthijscrafter@gmail.com>
2022-02-04 15:32:26 +0100

    Fixed spelling

--- beatrix-lib/locations.py
+++ beatrix-lib/locations.py
@@ -23,34 +23,34 @@ class Location:
     def get_name(self):
         return self.name
 
-INPUT_AREA_CAM_VIEW  = Location(85, 101, 60, 106, 90, "Input area cam view")
-INPUT_AREA_GRAB_CENTER = Location(84, 119, 82, 53, 90, "Input area grab center")
-PUZZLE_AREA_CAM_VIEW = Location(172, 94, 60, 107, 90, "Puzzle area cam view")
+INPUT_AREA_CAM_VIEW    = Location(85, 101, 60, 106, 90, "Input area cam view")
+INPUT_AREA_GRAB_CENTER = Location(84, 119, 82, 53,  90, "Input area grab center")
+PUZZLE_AREA_CAM_VIEW   = Location(172, 94, 60, 107, 90, "Puzzle area cam view")
 
 HOVER_ABOVE_PUZZLES = Location(172, 95, 58, 25, 90, "Hover above puzzle")
-HOVER_ABOVE_INPUT = Location(85, 121, 40, 10, 90, "Hover above input")
+HOVER_ABOVE_INPUT   = Location(85, 121, 40, 10, 90, "Hover above input")
 
 # ---- Puzzle locations
-OCTAGON = Location(175, 114, 88, 60, 90, "Octagon")
-ELLIPSE = Location(156, 126, 60, 41, 69, "Ellipse")
-SQUARE  = Location(148, 113, 79, 43, 68, "Square")
-CIRCLE  = Location(194.7, 142, 25.7, 31.7, 120, "Circle")
+OCTAGON    = Location(175, 114, 88, 60, 90, "Octagon")
+ELLIPSE    = Location(156, 126, 60, 41, 69, "Ellipse")
+SQUARE     = Location(148, 113, 79, 43, 68, "Square")
+CIRCLE     = Location(194.7, 142, 25.7, 31.7, 120, "Circle")
 SEMICIRCLE = Location(175, 102, 105, 60, 90, "Semicircle")
 TRIANGLE   = Location(206.8, 106, 89, 50, 120, "Triangle")
-RECTANLGE  = Location(198.4, 126, 59, 40, 115, "Rectangle")
-DIAMOND = Location(175, 130, 52, 48, 90, "Diamond")
-PENTAGON = Location(161, 138, 30, 34, 80, "Pentagon")
+RECTANGLE  = Location(198.4, 126, 59, 40, 115, "Rectangle")
+DIAMOND    = Location(175, 130, 52, 48, 90, "Diamond")
+PENTAGON   = Location(161, 138, 30, 34, 80, "Pentagon")
 
 PUZZLE_LOCATIONS = {
-    Shape.Octagon: OCTAGON,
-    Shape.Ellipse: ELLIPSE,
-    Shape.Square:  SQUARE,
-    Shape.Circle:  CIRCLE,
+    Shape.Octagon:    OCTAGON,
+    Shape.Ellipse:    ELLIPSE,
+    Shape.Square:     SQUARE,
+    Shape.Circle:     CIRCLE,
     Shape.Semicircle: SEMICIRCLE,
     Shape.Triangle:   TRIANGLE,
-    Shape.Rectangle:  RECTANLGE,
-    Shape.Diamond: DIAMOND,
-    Shape.Pentagon: PENTAGON,
+    Shape.Rectangle:  RECTANGLE,
+    Shape.Diamond:    DIAMOND,
+    Shape.Pentagon:   PENTAGON,
 }
 
 LOCATIONS_FOR_GUI = [
@@ -62,4 +62,3 @@ LOCATIONS_FOR_GUI = [
     TRIANGLE,
     OCTAGON
 ]
-
